ab0acdc5c8f28e7be4536a4454c763770da81166
vikipretium <vighneshpasupathy@gmail.com>
2022-01-28 15:41:58 +0530

    create bug

---
+++ main.js
@@ -0,0 +1,16 @@
+const calculateButton =
+  document.querySelector('.calculate');
+
+const firstNumber = document.querySelector('#first-number');
+
+const secondNumber = document.querySelector('#second-number');
+
+const resultField = document.querySelector('.res');
+
+const calculate = (e) => {
+  resultField.textContent =
+    firstNumber.value %++ secondNumber.value;
+  console.log(resultField.value);
+};
+
+calculateButton.addEventListener('click', calculate);
